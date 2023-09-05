255fd7c3392717ca54e716fe9b24cf6c65459113
lgwk42 <luisgunkim@gmail.com>
2023-09-05 15:32:15 +0900

    :fire: [Feature] :: 로그인 서비스 삭제 (구글 api 사용)

--- src/main/java/com/project/checkIn/domain/auth/presentation/AuthController.java
+++ src/main/java/com/project/checkIn/domain/auth/presentation/AuthController.java
@@ -1,15 +1,8 @@
 package com.project.checkIn.domain.auth.presentation;
 
-import com.project.checkIn.domain.auth.presentation.request.UserSignUpRequest;
-import com.project.checkIn.domain.auth.service.UserSignUpService;
-
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,11 +10,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final UserSignUpService userSignUpService;
-
-    @PostMapping("/user/sign-up")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void signUp(@RequestBody UserSignUpRequest request){
-        userSignUpService.execute(request);
-    }
 }
