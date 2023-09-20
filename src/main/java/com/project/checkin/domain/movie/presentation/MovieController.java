78cde549841501e15c7b1ed25883e8389b9397ad
lgwk42 <luisgunkim@gmail.com>
2023-09-20 12:18:53 +0900

    :sparkles: :: [Feature] :: 영화 컨트롤러 제작

--- src/main/java/com/project/checkin/domain/movie/presentation/MovieController.java
+++ src/main/java/com/project/checkin/domain/movie/presentation/MovieController.java
@@ -1,4 +1,30 @@
 package com.project.checkin.domain.movie.presentation;
 
+import com.project.checkin.domain.movie.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/movie")
 public class MovieController {
+
+    private final MovieService movieService;
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerMovie(){
+
+    }
+
+    @GetMapping("")
+    public void findMovie(){
+
+    }
+
 }
