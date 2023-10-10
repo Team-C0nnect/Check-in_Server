5116f36daf99628ee822ef35f72fe705b2a98aec
lgwk42 <luisgunkim@gmail.com>
2023-10-10 17:13:00 +0900

    :memo: :: [Feature] :: 오퍼레이션 어너테이션 추가 (영화 컨트롤러)

--- src/main/java/com/project/checkin/domain/movie/presentation/MovieController.java
+++ src/main/java/com/project/checkin/domain/movie/presentation/MovieController.java
@@ -4,6 +4,7 @@ import com.project.checkin.domain.movie.domain.MovieEntity;
 import com.project.checkin.domain.movie.dto.request.MovieRequest;
 import com.project.checkin.domain.movie.dto.response.MovieResponse;
 import com.project.checkin.domain.movie.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,12 +20,14 @@ public class MovieController {
 
     private final MovieService movieService;
 
+    @Operation(summary = "영화 등록", description = "")
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public void registerMovie(MovieRequest request, MovieEntity movie){
         movieService.registerMovie(request, movie);
     }
 
+    @Operation(summary = "영화 조회", description = "")
     @GetMapping("")
     public MovieResponse findMovie(MovieRequest request, MovieEntity movie){
         return movieService.findMovie(request,movie);
