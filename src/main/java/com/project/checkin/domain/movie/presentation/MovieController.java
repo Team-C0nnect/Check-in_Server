0402ab1259c55ffd910e8f158407ce15783515b0
lgwk42 <luisgunkim@gmail.com>
2023-10-10 17:15:17 +0900

    :memo: :: [Feature] :: 오퍼레이션 어너테이션 추가 (영화 컨트롤러)

--- src/main/java/com/project/checkin/domain/movie/presentation/MovieController.java
+++ src/main/java/com/project/checkin/domain/movie/presentation/MovieController.java
@@ -20,14 +20,14 @@ public class MovieController {
 
     private final MovieService movieService;
 
-    @Operation(summary = "영화 등록", description = "")
+    @Operation(summary = "영화 등록", description = "등록")
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public void registerMovie(MovieRequest request, MovieEntity movie){
         movieService.registerMovie(request, movie);
     }
 
-    @Operation(summary = "영화 조회", description = "")
+    @Operation(summary = "영화 조회", description = "조회")
     @GetMapping("")
     public MovieResponse findMovie(MovieRequest request, MovieEntity movie){
         return movieService.findMovie(request,movie);
