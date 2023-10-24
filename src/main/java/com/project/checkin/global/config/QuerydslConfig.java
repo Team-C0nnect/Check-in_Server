1cda9f7825fd743355d4da542385c1d93e6e9ff5
Sewook <sewook0303@dgsw.hs.kr>
2023-10-24 16:53:22 +0900

    :memo: [Feature] :: 충돌 코드 수정

--- src/main/java/com/project/checkin/global/config/QuerydslConfig.java
+++ src/main/java/com/project/checkin/global/config/QuerydslConfig.java
@@ -1,5 +1,6 @@
 package com.project.checkin.global.config;
 
+import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -13,7 +14,7 @@ public class QuerydslConfig {
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
-        return new JPAQueryFactory(entityManager);
+        return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
     }
 
 }
