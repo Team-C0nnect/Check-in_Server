11ba4685cf994d0d2d9bc8c812931cc14a9a17ca
lgwk42 <luisgunkim@gmail.com>
2023-10-24 17:16:46 +0900

    :memo: [Bug] :: 빌드 오류 수정 (getter 중복)

--- src/main/java/com/project/checkin/domain/movie/domain/MovieEntity.java
+++ src/main/java/com/project/checkin/domain/movie/domain/MovieEntity.java
@@ -1,13 +1,21 @@
 package com.project.checkin.domain.movie.domain;
 
 import com.project.checkin.global.entity.BaseTimeEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
 @AllArgsConstructor
-@Getter
 @Table(name = "tb_movie")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
