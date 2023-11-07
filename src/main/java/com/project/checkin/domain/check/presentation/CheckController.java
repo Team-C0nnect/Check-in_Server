148c23458ac13fe8c020079b24947170a8b65a56
Kim-Wongi-1 <120900266+Kim-Wongi-1@users.noreply.github.com>
2023-11-07 19:25:02 +0900

    📝 :: [Feature] :: ADD Check Code

--- src/main/java/com/project/checkin/domain/check/presentation/CheckController.java
+++ src/main/java/com/project/checkin/domain/check/presentation/CheckController.java
@@ -1,6 +1,8 @@
 package com.project.checkin.domain.check.presentation;
 
 import com.project.checkin.domain.check.dto.Check;
+import com.project.checkin.domain.check.dto.response.CheckCodeResponse;
+import com.project.checkin.domain.check.service.CheckCodeService;
 import com.project.checkin.domain.check.service.querydsl.CheckQueryService;
 import com.project.checkin.global.common.dto.request.PageRequest;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +26,17 @@ import java.util.List;
 public class CheckController {
 
     private final CheckQueryService checkQueryService;
+    private final CheckCodeService checkCodeService;
 
     @Operation(summary = "출석 명단", description = "출석 명단을 표시합니다")
     @GetMapping("/list")
     public ResponseEntity<List<Check>> findCheck(PageRequest pageRequest) {
         return ResponseEntity.status(HttpStatus.OK).body(checkQueryService.findCheck(pageRequest));
+    }
+
+    @Operation(summary = "출석 코드 생성", description = "출석 코드 생성합니다")
+    @PostMapping("/code")
+    public ResponseEntity<CheckCodeResponse> generateCheckCode() {
+        return ResponseEntity.status(HttpStatus.CREATED).body(checkCodeService.generate());
     }
 }
