a2754936171dd2394873fc2e66443fd44d08d655
lgwk42 <luisgunkim@gmail.com>
2023-11-07 20:12:39 +0900

    :bug: :: [Bug] :: 영화 등록 로직 오류 수정

--- src/main/java/com/project/checkin/domain/condition/domain/ConditionEntity.java
+++ src/main/java/com/project/checkin/domain/condition/domain/ConditionEntity.java
@@ -1,7 +1,14 @@
 package com.project.checkin.domain.condition.domain;
 
 import com.project.checkin.domain.condition.domain.enums.Feels;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
